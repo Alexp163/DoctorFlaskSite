ee3507ccfe623f4ed68cdedb48fce54bc3120398
Alexp163 <dr.p163@ya.ru>
2023-10-19 21:27:21 +0400

    Подключение БД: sqlalchemy, заполнение models.py

--- src/db.py
+++ src/db.py
@@ -1,1 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///slite.db"
+db = SQLAlchemy(app)
 
--- src/models.py
+++ src/models.py
@@ -1,2 +1,31 @@
-if __name__ == '__main__':
-    main()
+from db import db
+
+# id - (число)
+# first_name - имя (текст)
+# second_name - фамилия (текст)
+# last_name - отчество (текст)
+# profile - профиль (текст)
+# experience - опыт (число)
+# age - возраст (число)
+# telephone - телефон (текст)
+# email - почта (текст)
+# db.String(50) - текстовое поле заданной длины
+# db.Integer() - числовое поле
+# db.Column() - создания колонки (дополнительные поля nullable, unique, primary_key)
+# для id всегда задается primary_key=True
+
+class Doctor:
+    __tablename__ = "doctor"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    age = db.Column(db.Integer())
+
+    second_name = db.Column(db.String(50))
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50))
+
+    telephone = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+
+    profile = db.Column(db.String(50), nullable=False)
+    experience = db.Column(db.String(10))
