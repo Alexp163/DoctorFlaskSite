33c45677bb7a4f0c13cf336192370b1d77d7b143
Alexp163 <dr.p163@ya.ru>
2023-11-14 22:17:15 +0400

    Подключение ServiceGroup во view в service_group и index

--- src/models.py
+++ src/models.py
@@ -20,15 +20,19 @@ class Doctor(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     age = db.Column(db.Integer())
 
-    second_name = db.Column(db.String(50))
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50))
+    second_name = db.Column(db.String(50))  # фамилия
+    first_name = db.Column(db.String(50), nullable=False)  # имя
+    last_name = db.Column(db.String(50))  # отчество
 
-    telephone = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(50), nullable=False, unique=True)
+    telephone = db.Column(db.String(20), nullable=False)  # телефон
+    email = db.Column(db.String(50), nullable=False, unique=True)  # мыло
 
-    profile = db.Column(db.String(50), nullable=False)
-    experience = db.Column(db.String(10))
+    profile = db.Column(db.String(50), nullable=False)  # профиль
+    experience = db.Column(db.String(10))  # опыт работы
+
+    def __repr__(self):
+        return (f"< Фамилия: {self.second_name}, Имя: {self.first_name}, Отчество: {self.last_name},"
+                f" телефон: {self.telephone}, профиль: {self.profile}, опыт работы: {self.experience}>")
 
 
 class Service(db.Model):
@@ -40,8 +44,20 @@ class Service(db.Model):
     description = db.Column(db.Text())  # описание услуги
     executor = db.Column(db.String(50))  # исполнитель
     price = db.Column(db.Float())  # цена
-    service_grope_name = db.Column(db.String(50))  # из какой группы услуга
+    service_group_id = db.Column(db.ForeignKey("service_group.id"))  # id группы, к которой относится эта услуга
+    service_group = db.Relationship("ServiceGroup")  # ссылка на группу(id которой указан в вышестоящей строчке)
+    def __repr__(self):
+        return (f"<Наименование услуги: {self.name_service} описание услуги: {self.description} "
+                f"ответственный: {self.executor} цена: {self.price}>")
 
 
 
+class ServiceGroup(db.Model):
+    __tablename__ = "service_group"
+    id = db.Column(db.Integer(), primary_key=True)
 
+    name = db.Column(db.String(100))  # название группы
+    description = db.Column(db.Text())  # описание группы
+    # создать руководителя группы
+    def __repr__(self):
+        return f"<сервисная группа: {self.name}, описание группы: {self.description}>"
--- src/view.py
+++ src/view.py
@@ -1,12 +1,13 @@
 from app import app
 from flask import render_template
-from models import Service, Doctor
+from models import Service, Doctor, ServiceGroup
 
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    service_groups = ServiceGroup.query.all()
+    return render_template('index.html', service_groups=service_groups)
 
 
 @app.route('/doctors')
@@ -35,3 +36,12 @@ def one_doctor(doctor_id):
 def service():
     service = Service.query.all()
     return render_template('analyzes.html', service=service)
+
+
+@app.route('/service_group/<int:service_group_id>')
+def service_group(service_group_id):
+    service_group = ServiceGroup.query.get(service_group_id)
+    service_groups = ServiceGroup.query.all()
+    service = Service.query.filter_by(service_group_id=service_group_id).all()
+    return render_template("service_group.html", service_group=service_group,
+                           service=service, service_groups=service_groups)
